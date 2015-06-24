766c601d42673d57ee5191f2905fbe6b760ec3a8
Tristan Rubadeau <tristan@rubadeau.com>
2015-06-23 21:06:01 -0600

    WIP

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -10,14 +10,18 @@ class TransactionsController < ApplicationController
 
   def create
     @transaction = Transaction.new(transaction_params)
-    fireflyRequest = RestClient.get(build_touchsuite_url_for_transaction)
+    fireflyRequest = RestClient.get(firefly_request_url)
     fireflyResponse = JSON.parse fireflyRequest, symbolize_names:true
     @transaction.update_attribute(:auth_code, fireflyResponse.first[:authorization_id_response])
     if @transaction.save
-      flash[:notice] = "Waahoo! Transaction was approved for #{@transaction.amount}. Your Auth Code is #{@transaction.auth_code}"
+        if @transaction.auth_code.present?
+          flash[:success] = "Transaction was approved for #{@transaction.amount}. Your Auth Code is #{@transaction.auth_code}"
+        else
+          flash[:danger] = "Transaction Declined"
+        end
       redirect_to transactions_path
     else
-      flash[:notice] = "Whoops, something went wrong."
+      flash[:notice] = "Whoops, something went wrong. Try again"
       render :new
     end
   end
@@ -28,8 +32,7 @@ class TransactionsController < ApplicationController
     params.require(:transaction).permit(:card_number, :exp_date, :cvv, :amount)
   end
 
-  def build_touchsuite_url_for_transaction
+  def firefly_request_url
    "https://cloud.touchsuite.com/api/mobile/process_manual_credit_card.json?api_key=046AAA0614C811E389AED4BED9E2D958&active=true&authcode=&cc_holder=&amount=#{@transaction.amount}&cardnumber=#{@transaction.card_number}&expiry=#{@transaction.exp_date}"
   end
-
 end
